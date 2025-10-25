ce49b9026946d6d3e5a67df4857fe90310f0ba80
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-10-25 13:19:14 +0100

    first commit

---
+++ src/theme/theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "1976d2",
+        },
+        secondary: {
+            main: "ff4081",
+        }, background: {
+            default: "#99f9f9",
+        },
+      },
+      typography: {
+        fontFamily: "'Outfit', sans-serif"
+      },
+});
+
+export default theme;
